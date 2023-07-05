4721a0f57f7b9dec8072732781168b2dc46957d8
Raiyan Memon <raiyan.appdid@gmail.com>
2023-07-05 16:39:48 +0530

    updated

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -4,13 +4,15 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Suspense } from 'react'
 import MovieCard from "../components/moviecard";
-export default function Page() {
+
+
+export default function page() {
 
     const IMGPATH = "https://image.tmdb.org/t/p/w1280";
     const [movieArray, movieUpdate] = useState([]);
     const [page, updatePage] = useState(1);
 
-    function GetData(count) {
+    function getData(count) {
         axios({
             url: `https://api.themoviedb.org/3/movie/popular?api_key=6d02a218c581074ce22ac8d31f03aaf7&page=${count}`,
             method: "GET"
@@ -25,19 +27,19 @@ export default function Page() {
     }
 
     React.useEffect(() => {
-        GetData(1);
+        getData(1);
     }, []);
 
-    function PageIncrement() {
+    function pageIncrement() {
         let count = page + 1;
         updatePage(count)
-        GetData(count)
+        getData(count)
     }
-    function PageDecrement() {
+    function pageDecrement() {
         if (page > 1) {
             let count = page - 1;
             updatePage(count)
-            GetData(count)
+            getData(count)
         }
     }
 
@@ -54,13 +56,13 @@ export default function Page() {
             </div>
             <div className="row justify-content-center">
                 <div className="col-md-1 col-2 text-end">
-                    <button onClick={PageDecrement} className="btn btn-outline-danger">-</button>
+                    <button onClick={pageDecrement} className="btn btn-outline-danger">-</button>
                 </div>
                 <div className="col-md-1 col-2 text-center">
                     <p>{page}</p>
                 </div>
                 <div className="col-md-1 col-2 text-left">
-                    <button onClick={PageIncrement} className="btn btn-outline-success">+</button>
+                    <button onClick={pageIncrement} className="btn btn-outline-success">+</button>
                 </div>
             </div>
         </div >
