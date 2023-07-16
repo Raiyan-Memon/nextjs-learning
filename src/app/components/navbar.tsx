310f4db5fe4fc2edba95e002678b56c642c9b004
Raiyan Memon <raiyan.appdid@gmail.com>
2023-07-16 20:36:01 +0530

    updated

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     const pathname = usePathname()
 
     return (
-        <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
