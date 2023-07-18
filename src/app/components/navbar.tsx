474aff196ee42fe92b2be546bb5a33f12a6f21e1
Raiyan Memon <raiyan.appdid@gmail.com>
2023-07-18 23:02:25 +0530

    updated

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,35 +6,39 @@ import { usePathname } from 'next/navigation';
 export default function Navbar() {
     const pathname = usePathname()
 
+    function CloseNavbar(){
+        const navbar = document.getElementById('collapse-navbar')?.click();
+    }
+
     return (
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" id='collapse-navbar' data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" onClick={CloseNavbar}>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/" ? ' text-black' : '')} aria-current="page" href="/">Home</Link>
+                            <Link className={'nav-link' + (pathname == "/" ? ' text-black' : '')} aria-current="page" href="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/about" ? ' text-black' : '')} href="/about">Weather</Link>
+                            <Link className={'nav-link' + (pathname == "/about" ? ' text-black' : '')} href="/about">Weather</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/contact" ? ' text-black' : '')} href="/contact">Todo's App</Link>
+                            <Link className={'nav-link' + (pathname == "/contact" ? ' text-black' : '')} href="/contact">Todo's App</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname.indexOf('pricing') >= 0 ? ' text-black' : '')} href="/pricing/1">Movie</Link>
+                            <Link className={'nav-link' + (pathname.indexOf('pricing') >= 0 ? ' text-black' : '')} href="/pricing/1">Movie</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/service" ? ' text-black' : '')} href="/service">Service</Link>
+                            <Link className={'nav-link' + (pathname == "/service" ? ' text-black' : '')} href="/service">Service</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/login" ? ' text-black' : '')} href="/login">LogIN</Link>
+                            <Link className={'nav-link' + (pathname == "/login" ? ' text-black' : '')} href="/login">LogIN</Link>
                         </li>
                         <li className="nav-item">
-                            <Link data-bs-toggle="collapse" className={'nav-link' + (pathname == "/signup" ? ' text-black' : '')} href="/signup">SignUp</Link>
+                            <Link className={'nav-link' + (pathname == "/signup" ? ' text-black' : '')} href="/signup">SignUp</Link>
                         </li>
 
                     </ul>
